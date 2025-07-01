f685238edd46361c3890a5a6edb2282142e0e170
TrinhDinhHoang29 <162282399+TrinhDinhHoang29@users.noreply.github.com>
2025-07-01 12:41:03 +0700

    #develop feat:

--- vite.config.ts
+++ vite.config.ts
@@ -24,4 +24,12 @@ export default defineConfig({
   optimizeDeps: {
     include: ["react", "react-dom"],
   },
+  server: {
+    port: 3000,
+    allowedHosts: ["0.0.0.0", "0.0.0.0"],
+  },
+  preview: {
+    port: 3000,
+    allowedHosts: ["0.0.0.0", "0.0.0.0"],
+  },
 });
