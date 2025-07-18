f074565814956efa9d13785adbf1dd5ffbfab1f2
TrinhDinhHoang29 <162282399+TrinhDinhHoang29@users.noreply.github.com>
2025-07-18 09:39:57 +0700

    #main improve: fix notification warehouse

--- src/components/warehouses/notification-detail-warehouse.tsx
+++ src/components/warehouses/notification-detail-warehouse.tsx
@@ -8,22 +8,22 @@ const NotifycationDetailWarehouse = ({
   warehouse,
 }: NotifycationDetailWarehouseProps) => {
   return (
-    <div className="my-3">
-      <div className="bg-orange-50 border border-orange-200 rounded-md p-4 flex gap-x-4">
-        <div className="">
-          <AlertCircleIcon className="h-5 w-5 text-orange-500" />
-        </div>
-        <div>
-          <h3 className="text-sm font-bold text-orange-800">Cảnh báo</h3>
-          {(!warehouse.description || !warehouse.stockPlace) && (
+    (!warehouse.description || !warehouse.stockPlace) && (
+      <div className="my-3">
+        <div className="bg-orange-50 border border-orange-200 rounded-md p-4 flex gap-x-4">
+          <div className="">
+            <AlertCircleIcon className="h-5 w-5 text-orange-500" />
+          </div>
+          <div>
+            <h3 className="text-sm font-bold text-orange-800">Cảnh báo</h3>
             <div className="mt-1 text-sm text-orange-700">
               Mô tả hoặc vị trí của lô hàng này chưa được thêm , hãy thêm ngay
               để tránh sai sót không đáng có
             </div>
-          )}
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
